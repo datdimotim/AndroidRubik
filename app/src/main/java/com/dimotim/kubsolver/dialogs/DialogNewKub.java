1ddd987434efe1022c60959b97d6c87ac5a5539d
dimotim <datdimotim@gmail.com>
2021-10-07 23:03:34 +0300

    DialogNewKub> cleanup code

--- app/src/main/java/com/dimotim/kubsolver/dialogs/DialogNewKub.java
+++ app/src/main/java/com/dimotim/kubsolver/dialogs/DialogNewKub.java
@@ -10,7 +10,6 @@ import com.dimotim.kubsolver.MainActivity;
 
 public class DialogNewKub extends DialogFragment {
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        final int[] n = {-1};
         String[] strings = new String[127];
         for (int i = 0; i < strings.length; i++) {
             strings[i] = (i + 2) + "x" + (i + 2) + "x" + (i + 2);
@@ -19,8 +18,7 @@ public class DialogNewKub extends DialogFragment {
                 .setTitle("New Cube")
                 .setNegativeButton("Cancel", (dialog, id) -> dialog.cancel())
                 .setItems(strings, (dialog, which) -> {
-                    n[0] = which + 2;
-                    ((MainActivity) getActivity()).renderer.setNewKub(n[0]);
+                    ((MainActivity) getActivity()).renderer.setNewKub(which + 2);
                 })
                 .create();
     }
